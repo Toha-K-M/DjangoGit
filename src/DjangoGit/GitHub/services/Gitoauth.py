31c9a457cfff9810dca2d541ac4a97eb36497060
Toha <senor.toha@gmail.com>
2020-04-20 22:32:34 +0600

    # Spec 3: stored oAuth credentials, access_key_is_valid service

--- src/DjangoGit/GitHub/services/Gitoauth.py
+++ src/DjangoGit/GitHub/services/Gitoauth.py
@@ -4,8 +4,33 @@ import requests, json
 from service_objects.services import Service
 from rest_framework.exceptions import PermissionDenied
 from ... import application_properties
+# from django.contrib.auth.models import User
 
 logger = logging.getLogger("error_logger")
+
+class access_token_is_valid(Service):
+    def service_clean(self):
+        current_user = self.files.get("current_user")
+        if current_user is None or current_user.is_authenticated is False:
+            raise PermissionDenied()
+
+    def process(self):
+        try:
+            current_user = self.files.get("current_user")
+            if current_user.gitprofile.access_token:
+                api = 'https://api.github.com/user'
+                access_token = current_user.gitprofile.access_token
+                response = requests.get(api, headers={'Authorization': 'token '+access_token})
+                if response.status_code == 401:
+                    print('Bad Credentials')
+                    return False
+                return True
+            else:
+                return False
+
+        except Exception as e:
+            logger.error(repr(e))
+            return None
 
 class get_access_token(Service):
     def service_clean(self):
@@ -15,6 +40,14 @@ class get_access_token(Service):
 
     def process(self):
         try:
+            current_user = self.files.get("current_user")
+
+            def save_current_user_oauth(response):
+                current_user.gitprofile.access_token = response['access_token']
+                current_user.gitprofile.token_type = response['token_type']  
+                current_user.gitprofile.scope = response['scope']      
+                current_user.save()
+
             url = "https://github.com/login/oauth/access_token"
             code = self.data.GET['code']
             payload = {
@@ -24,10 +57,9 @@ class get_access_token(Service):
             }
             headers = {'Accept': 'application/json'}
             response = requests.post(url, payload, headers=headers).json()
-            access_token = response['access_token']
-            get_response = requests.get('https://api.github.com/user', 
-                headers={'Authorization': 'token '+access_token})
-            # print(get_response.json())
+            
+            save_current_user_oauth(response)
+
             return True
 
         except Exception as e:
--- src/DjangoGit/GitHub/views/HomeView.py
+++ src/DjangoGit/GitHub/views/HomeView.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from DjangoGit.users.views import RegisterView
+from ..services import Gitoauth
 # Create your views here.
 def Home(request):
     if request.user.is_authenticated:
-        return render(request, 'GitHub/home.html')
+        is_linked = Gitoauth.access_token_is_valid.execute(request, {"current_user":request.user})
+        if is_linked:
+            return redirect('git_public_repos')
+        else:
+            return render(request, 'GitHub/home.html')
     else:
         return redirect(RegisterView.register)
 
